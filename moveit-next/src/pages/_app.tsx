dabc67bd0ee1e856334efd776057478b090d8e6f
Willian Endo <whke.endo33@gmail.com>
2021-03-03 11:58:52 -0300

    Last Commit

---
+++ moveit-next/src/pages/_app.tsx
@@ -0,0 +1,12 @@
+import '../styles/global.css'
+
+import { ChallengesContext, ChallengesProvider } from '../contexts/ChallengesContext';
+import { useState } from 'react';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Component {...pageProps} />
+  )
+}
+
+export default MyApp
